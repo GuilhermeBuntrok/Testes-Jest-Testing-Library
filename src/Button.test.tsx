76df7bcd3a2b5711ffba6251b756e8502cf030b0
GuilhermeBuntrok <guilhermebuntrock@hotmail.com>
2023-04-25 15:44:44 -0300

    :sparkles: feat Test  components

---
+++ src/Button.test.tsx
@@ -0,0 +1,26 @@
+import { fireEvent, render, screen } from "@testing-library/react"
+import Button from "./components/Button"
+
+describe('Button Component', () => {
+  it('should render with red background if disabled', () => {
+    render(<Button disabled={false} onClick={() => { }}>Click me</Button>)
+
+    const button = screen.getByRole('button', { name: 'Click me' })
+
+    expect(button).toHaveStyle({ backgroundColor: 'blue' })
+  })
+
+  it('should call onCLick prop on click', () => {
+    const onClick = jest.fn()
+
+    render(<Button disabled onClick={onClick}>
+      Click me
+    </Button>)
+
+    const button = screen.getByText(/click me/i)
+
+    fireEvent.click(button)
+
+    expect(onClick).toHaveBeenCalled()
+  })
+})
